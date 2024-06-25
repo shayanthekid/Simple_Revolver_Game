d5f01bec0ae96f83c5a0a54d9e8af6cc9e90630e
sajid LAp <sajidfayazhaniff@gmail.com>
2024-06-25 11:26:39 +0530

    added bullets, input field

--- JS/script.js
+++ JS/script.js
@@ -11,3 +11,18 @@ function flipRevolver(){
     }
 
 }
+
+
+
+  // Example of how to dynamically show/hide images using JavaScript
+  function showImage(imageId) {
+    document.getElementById(imageId).style.display = 'flex';
+}
+
+function hideImage(imageId) {
+    document.getElementById(imageId).style.display = 'none';
+}
+
+// Example usage
+// hideImage('image1');
+// showImage('image1');
